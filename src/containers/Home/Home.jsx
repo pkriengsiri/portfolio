bf3a79a4cd694849e7eb031b71102b87a3145f59
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-03-09 18:46:10 -0500

    Added link to devicons cdn

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -2,41 +2,38 @@ import React from "react";
 import "./Home.css";
 import { useHistory } from "react-router-dom";
 
-
 const Home = () => {
-
   const history = useHistory();
 
   const linkAbout = () => {
-    history.push(`/`);
-  }
+    history.push(`/about`);
+  };
 
   const linkPortfolio = () => {
     history.push(`/portfolio`);
-  }
+  };
 
   return (
     <div className="container mt-5 home">
+      <div id="welcome-message">Welcome to my portfolio</div>
       <div className="row">
         <div className="col-12 col-md-6">
-          <div id="welcome-message">Welcome to my portfolio</div>
+          
           <h3 className="mt-3">
             I'm a full-stack developer based in Atlanta, GA
           </h3>
           <div className="row">
-            <div className="col-5 home-buttons m-3" onClick={linkAbout}>About me</div>
-
-            <div className="col-5 home-buttons m-3" onClick={linkPortfolio}>View my projects</div>
-
-            {/* <div className="col-5 home-buttons m-3">About me</div>
-             <div className="col-5 home-buttons m-3">View my projects</div> */}
+            <div className="col-5 home-buttons m-3" onClick={linkAbout}>
+              About me
+            </div>
+            <div className="col-5 home-buttons m-3" onClick={linkPortfolio}>
+              View my projects
+            </div>
           </div>
-          {/* <button>About me</button>
-          <button>View my projects</button> */}
         </div>
         <div className="col-12 col-md-6">
-          <h2>Connect with me:</h2>
-          <div className="row">
+          <h2 className="mt-2">Connect with me:</h2>
+          <div className="row ml-2">
             <a
               href="https://github.com/pkriengsiri"
               target="_blank"
@@ -55,7 +52,7 @@ const Home = () => {
               <i className="fas fa-envelope-square fa-6x"></i>
             </a>
           </div>
-          <h2>My technologies:</h2>
+          <h2 className="mt-3">My technologies:</h2>
         </div>
       </div>
     </div>
