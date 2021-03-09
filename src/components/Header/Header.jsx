a1b22bd39d9b6d277e4c6d47f26869e9066fc9ee
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-03-09 18:31:09 -0500

    Updated navbar links

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
-import resume from "../../files/dpk_resume.pdf"
+import resume from "../../files/dpk_resume.pdf";
 
 const Header = () => {
   const handleClick = () => {
@@ -12,14 +12,16 @@ const Header = () => {
     <>
       <nav className="navbar navbar-expanded">
         <div className="col-4">
-          <Link to="/" className="h3 mr-4">About</Link>
-          <Link to="/portfolio" className="h3 mr-4">Portfolio</Link>
-          <a
-            href={resume}
-            target="_blank"
-            rel="noreferrer"
-            className="h3"
-          >
+          <Link to="/" className="h3 mr-4">
+            Home
+          </Link>
+          <Link to="/about" className="h3 mr-4">
+            About
+          </Link>
+          <Link to="/portfolio" className="h3 mr-4">
+            Portfolio
+          </Link>
+          <a href={resume} target="_blank" rel="noreferrer" className="h3">
             Resume
           </a>
         </div>
@@ -31,15 +33,23 @@ const Header = () => {
           </div>
         </div>
         <div className="col-4 d-flex justify-content-end">
-          <a href="https://github.com/pkriengsiri" target="_blank" rel="noreferrer">
+          <a
+            href="https://github.com/pkriengsiri"
+            target="_blank"
+            rel="noreferrer"
+          >
             <i className="fab fa-github-square fa-3x mr-2"></i>
           </a>
-          <a href="https://www.linkedin.com/in/petekriengsiri/" target="_blank" rel="noreferrer">
+          <a
+            href="https://www.linkedin.com/in/petekriengsiri/"
+            target="_blank"
+            rel="noreferrer"
+          >
             <i className="fab fa-linkedin fa-3x mr-2"></i>
           </a>
           <a href="mailto:pkriengsiri@gmail.com">
-          <i className="fas fa-envelope-square fa-3x"></i>
-        </a>
+            <i className="fas fa-envelope-square fa-3x"></i>
+          </a>
         </div>
       </nav>
       <nav className="navbar navbar-collapse">
