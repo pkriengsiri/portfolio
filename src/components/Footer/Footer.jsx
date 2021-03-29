7129c2a2828f8af1e1fd69e59d362822dc150eee
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-03-28 20:56:28 -0400

    Updated copyright date in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="footer pt-1 mt-3">
       <div className="container-fluid text-center">
-        <span>&#169; Copyright 2020 Pete Kriengsiri</span><span> - View the <a href="https://github.com/pkriengsiri/portfolio" target="_blank" rel="noreferrer">source code</a> for this site</span>
+        <span>&#169; Copyright 2021 Pete Kriengsiri</span><span> - View the <a href="https://github.com/pkriengsiri/portfolio" target="_blank" rel="noreferrer">source code</a> for this site</span>
       </div>
     </footer>
   );
