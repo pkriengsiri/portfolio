4ac48e6a7e7794046897f8ef9429335ff5b6d012
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-05-09 21:16:28 -0400

    Fixed spacing issues on home page

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -27,52 +27,54 @@ const Home = () => {
             </h2>
             <div className="row ml-3">
               <div
-                className="col-5 home-buttons m-1 text-center"
+                className="col-5 home-buttons mr-1 text-center"
                 onClick={linkAbout}
               >
                 <p className="home-buttons-text">About me</p>{" "}
                 <i className="fas fa-id-card fa-4x"></i>
               </div>
               <div
-                className="col-5 home-buttons m-1 text-center"
+                className="col-5 home-buttons ml-1 text-center"
                 onClick={linkPortfolio}
               >
-                <p className="home-buttons-text">View my projects</p>
+                <p className="home-buttons-text">My projects</p>
                 <i className="fas fa-tools fa-4x"></i>
               </div>
             </div>
-            <div className="my-4">
-              <h2 className="mx-2">Connect with me:</h2>
-              <div className="row ml-4 d-flex ">
-                <div className="connect-icons p-3">
-                  <a
-                    href="https://github.com/pkriengsiri"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="home-link"
-                    aria-label="Link to Github"
-                  >
-                    <i className="fab fa-github-square fa-5x mr-2"></i>
-                  </a>
-                  <a
-                    href="https://www.linkedin.com/in/petekriengsiri/"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="home-link"
-                    aria-label="Link to LinkedIn"
-                  >
-                    <i className="fab fa-linkedin fa-5x mr-2"></i>
-                  </a>
-                  <a href="mailto:pkriengsiri@gmail.com" className="home-link" aria-label="Email Link">
-                    <i className="fas fa-envelope-square fa-5x"></i>
-                  </a>
-                </div>
+            <h2 className="mx-2 my-3">Connect with me:</h2>
+            <div className="row ml-3">
+              <div className="connect-icons p-3">
+                <a
+                  href="https://github.com/pkriengsiri"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="home-link"
+                  aria-label="Link to Github"
+                >
+                  <i className="fab fa-github-square fa-5x mr-2"></i>
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/petekriengsiri/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="home-link"
+                  aria-label="Link to LinkedIn"
+                >
+                  <i className="fab fa-linkedin fa-5x mr-2"></i>
+                </a>
+                <a
+                  href="mailto:pkriengsiri@gmail.com"
+                  className="home-link"
+                  aria-label="Email Link"
+                >
+                  <i className="fas fa-envelope-square fa-5x"></i>
+                </a>
               </div>
             </div>
           </div>
           <div className="col-12 col-lg-6">
             <h2 className="mt-3 mb-3">My technologies:</h2>
-            <div className="row icons p-3 d-flex justify-content-center mx-2">
+            <div className="row icons p-2 d-flex justify-content-center mx-3">
               <i className="devicon-mongodb-plain-wordmark devicon m-2"></i>
               <i className="devicon-express-original devicon m-2"></i>
               <i className="devicon-react-original-wordmark devicon m-2"></i>
