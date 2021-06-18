7533721d79fdd42ea156389b6fe203cac8cd89f0
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-06-17 21:28:37 -0400

    Added aria labels for devicons

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -75,20 +75,20 @@ const Home = () => {
           <div className="col-12 col-lg-6">
             <h2 className="mt-3 mb-3 mx-2">My technologies:</h2>
             <div className="row icons p-2 d-flex justify-content-center mx-3">
-              <i className="devicon-mongodb-plain-wordmark devicon m-2"></i>
-              <i className="devicon-express-original devicon m-2"></i>
-              <i className="devicon-react-original-wordmark devicon m-2"></i>
-              <i className="devicon-nodejs-plain devicon m-2"></i>
-              <i className="devicon-javascript-plain devicon m-2"></i>
-              <i className="devicon-jquery-plain-wordmark devicon m-2"></i>
-              <i className="devicon-html5-plain-wordmark devicon m-2"></i>
-              <i className="devicon-mysql-plain-wordmark devicon m-2"></i>
-              <i className="devicon-handlebars-plain-wordmark devicon m-2"></i>
-              <i className="devicon-sequelize-plain-wordmark devicon m-2"></i>
-              <i className="devicon-bootstrap-plain-wordmark devicon m-2"></i>
-              <i className="devicon-css3-plain-wordmark devicon m-2"></i>
-              <i className="devicon-git-plain-wordmark devicon m-2"></i>
-              <i className="devicon-github-original-wordmark devicon m-2"></i>
+              <i className="devicon-mongodb-plain-wordmark devicon m-2" aria-label="MongoDB icon"></i>
+              <i className="devicon-express-original devicon m-2" aria-label="Express icon"></i>
+              <i className="devicon-react-original-wordmark devicon m-2" aria-label="React icon"></i>
+              <i className="devicon-nodejs-plain devicon m-2" aria-label="Node.js icon"></i>
+              <i className="devicon-javascript-plain devicon m-2" aria-label="JavaScript icon"></i>
+              <i className="devicon-jquery-plain-wordmark devicon m-2" aria-label="jQuery icon"></i>
+              <i className="devicon-html5-plain-wordmark devicon m-2" aria-label="HTML5 icon"></i>
+              <i className="devicon-mysql-plain-wordmark devicon m-2" aria-label="MySQL icon"></i>
+              <i className="devicon-handlebars-plain-wordmark devicon m-2" aria-label="Handlebars icon"></i>
+              <i className="devicon-sequelize-plain-wordmark devicon m-2" aria-label="Sequelize icon"></i>
+              <i className="devicon-bootstrap-plain-wordmark devicon m-2" aria-label="Bootstrap icon"></i>
+              <i className="devicon-css3-plain-wordmark devicon m-2" aria-label="CSS3 icon"></i>
+              <i className="devicon-git-plain-wordmark devicon m-2" aria-label="Git icon"></i>
+              <i className="devicon-github-original-wordmark devicon m-2" aria-label="Github icon"></i>
             </div>
           </div>
         </div>
